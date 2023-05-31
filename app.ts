f3ff822d1f990fb6e7869991d753047dade30b63
Jim Kang <jimkang@gmail.com>
2023-05-30 22:33:51 -0400

    Add bad mouse-driven rotation.

--- app.ts
+++ app.ts
@@ -6,9 +6,11 @@ import { renderTriangle } from './renderers/render-triangle';
 import { renderTexture } from './renderers/render-texture';
 import { renderTransforms } from './renderers/render-tranforms';
 import { renderRotation } from './renderers/render-rotation';
+import { Pt3 } from './types';
 
 var routeState;
 var textureImages: TexImageSource[] = [];
+var currentRotation: Pt3 = { x: 0, y: 0, z: 0 };
 
 (async function go() {
   window.onerror = reportTopLevelError;
@@ -43,11 +45,41 @@ async function followRoute({ seed }) {
     textureImages.push(image);
     renderTexture({ canvasSel: '#texture-canvas', textureImages });
     renderTransforms({ canvasSel: '#transforms-canvas', textureImages });
+    scheduleRenderRotation();
+    wireControls();
+  }
+}
+
+function scheduleRenderRotation() {
+  requestAnimationFrame(() =>
     renderRotation({
       canvasSel: '#rotation-canvas',
       textureImages,
-      rotation: { x: Math.PI, y: 0, z: Math.PI / 4 },
-    });
+      rotation: currentRotation, //{ x: Math.PI, y: -1, z: Math.PI / 4 },
+    })
+  );
+}
+
+function wireControls() {
+  // Change the rotation when the mouse is clicked inside the rotation canvas
+  // then dragged.
+  var rotationCanvas = document.getElementById('rotation-canvas');
+  rotationCanvas.addEventListener('mousedown', onRotationMouseDown);
+
+  function onRotationMouseDown() {
+    document.body.addEventListener('mouseup', onBodyMouseUp, { once: true });
+    document.body.addEventListener('mousemove', onBodyMouseMove);
+  }
+
+  function onBodyMouseUp() {
+    document.body.removeEventListener('mousemove', onBodyMouseMove);
+  }
+
+  function onBodyMouseMove(event) {
+    // TODO: Find out what the movement units actually are.
+    currentRotation.y += event.movementX;
+    currentRotation.x += event.movementY;
+    scheduleRenderRotation();
   }
 }
 
